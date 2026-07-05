28e888cd7534b8a17cc00ddfda8387cfc41dbd20
itzRJ2 <loganfaithusa19@gmail.com>
2026-07-04 18:24:07 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: " You ",
+        name: " Watching You ",
         // Activity type number (0 = Playing).
-        type: 3,
+        type: 1,
       },
     ],
   },
